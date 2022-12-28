23e250c7556df8591763a96a9c8f1a1acfba321d
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2022-12-28 11:27:27 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+const displayedWordEl = document.getElementById('displayed-word');
+const remainingGuessesEl = document.getElementById('remaining-guesses');
+
+document.getElementById('guess-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Récupérez la lettre devinée à partir du formulaire
+  const letter = document.getElementById('letter').value;
+
+  // Envoyez une requête POST à la route '/guess' avec la lettre devinée
+  fetch('/guess', {
+    method: 'POST',
+    body: JSON.stringify({ letter }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      // Affichez le résultat de la requête
+      alert(result);
+
+      // Mettez à jour l'affichage du mot à deviner et du nombre de coups restants
+      displayedWordEl.textContent = `Mot à deviner : ${displayedWord}`;
+      remainingGuessesEl.textContent = `Coups restants : ${remainingGuesses}`;
+    });
+});
